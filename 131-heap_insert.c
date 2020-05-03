f35dcdf020588bf4a37f091e5ca79fb4f6ff1f71
PauloMorillo <pauloan@hotmail.com>
2020-05-03 04:15:24 +0000

    [update] only add right if is perfect left

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -2,6 +2,8 @@
 #include "binary_trees.h"
 #include <stdio.h>
 #include "binary_trees.h"
+int binary_tree_heighta(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 /**
  * heap_insert - Function to insert a node for a binary tree
@@ -59,13 +61,14 @@ binary_tree_t *chooseparent(binary_tree_t *tree)
 		if (tree->left)
 			return (chooseparent(tree->left));
 	}
-	else if (a > b && binary_tree_balance(tree->left) != 0)
+	else if (a > b && binary_tree_is_perfect(tree->left) == 0)
 	{
 		if (tree->left)
 			return (chooseparent(tree->left));
 	}
 	else
 	{
+		printf("esto es balance %d", binary_tree_balance(tree->left));
 		if (tree->right)
 			return (chooseparent(tree->right));
 	}
@@ -77,7 +80,7 @@ binary_tree_t *chooseparent(binary_tree_t *tree)
  * @tree: root of tree
  * Return: returns the size
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+int binary_tree_size(const binary_tree_t *tree)
 {
 	/* int c = 0; */
 	int a = 0;
@@ -126,26 +129,59 @@ binary_tree_t *verify_order(binary_tree_t *tree)
 }
 
 /**
- * binary_tree_balance - returns the balance (left - right) of the tree
+ * binary_tree_heighta - measures the height of a binary tree
  * @tree: root of tree
- * Return: returns the balance
+ * Return: returns the height of the node
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_heighta(const binary_tree_t *tree)
 {
-	int l = -1;
-	int r = -1;
+	int a = 0;
+	int b = 0;
+	/* int c = 0; */
 
 	if (tree)
 	{
 		if (tree->left)
-		{
-			l = (binary_tree_balance(tree->left));
-		}
+			a = (1 + binary_tree_heighta(tree->left));
 		if (tree->right)
+			b = (1 + binary_tree_heighta(tree->right));
+		if (a < b)
+			return (b);
+		else
+			return (a);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_is_perfect - returns 1 if the tree is full or 0 if not
+ * @tree: root of tree
+ * Return: returns the balance
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int h = 0;
+	int ful = 1;
+	int h2 = 0;
+	int fulpo = 0;
+
+	if (tree)
+	{
+		h = binary_tree_heighta(tree);
+		while (h > 0)
 		{
-			r = (binary_tree_balance(tree->right));
+			ful = 1;
+			h2 = h;
+			while (h2 > 0)
+			{
+				ful = ful * (2);
+				h2 = h2 - 1;
+			}
+			fulpo = fulpo + ful;
+			h = h - 1;
 		}
-		return (l - r);
+		if (fulpo + 1 == binary_tree_size(tree))
+			return (1);
 	}
 	return (0);
 }
